8a0a53d45c0367e8a3fd3bf867ee2680c18c038c
RayanCooray <raycooray32@gmail.com>
2026-08-27 15:28:48 +0530

    feat: register dao factory services

--- src/main/java/lk/sunrise/dentalclinic/factory/DAOFactory.java
+++ src/main/java/lk/sunrise/dentalclinic/factory/DAOFactory.java
@@ -6,30 +6,39 @@ import lk.sunrise.dentalclinic.dao.impl.*;
 public final class DAOFactory {
     private DAOFactory() {
     }
+
     public static UserDAO userDAO() {
         return new UserDAOImpl();
     }
+
     public static PatientDAO patientDAO() {
         return new PatientDAOImpl();
     }
+
     public static DentistDAO dentistDAO() {
         return new DentistDAOImpl();
     }
+
     public static TreatmentDAO treatmentDAO() {
         return new TreatmentDAOImpl();
     }
+
     public static AppointmentDAO appointmentDAO() {
         return new AppointmentDAOImpl();
     }
+
     public static TreatmentRecordDAO treatmentRecordDAO() {
         return new TreatmentRecordDAOImpl();
     }
+
     public static InvoiceDAO invoiceDAO() {
         return new InvoiceDAOImpl();
     }
+
     public static PaymentDAO paymentDAO() {
         return new PaymentDAOImpl();
     }
+
     public static ReportDAO reportDAO() {
         return new ReportDAOImpl();
     }
